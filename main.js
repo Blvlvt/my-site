4e5c1753a021910af56435640d08b7610ad70831
Blvlvt <60438587+Blvlvt@users.noreply.github.com>
2020-01-29 18:42:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+const inpX = document.querySelector('.x'),
+    inpY = document.querySelector('.y'),
+    ball = document.querySelector('.ball');
+ball.addEventListener('click', function () {
+    this.style.left = inpX.value + 'px';
+    this.style.top  = inpY.value + 'px';
+});
+
+let button = document.querySelector('.btn');
+button.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    let inpColor = document.querySelector('.color');
+    console.log(inpColor.value);
+    ball.style.backgroundImage = 'none';
+    ball.style.backgroundColor = inpColor.value;
+});
+
+let btnGradient = document.querySelector('.btnGradient');
+
+btnGradient.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
+    let inputs = document.querySelectorAll('.colorGR');
+    let colors = [];
+    for( let i = 0; i < inputs.length; i++ ){
+        colors.push( inputs[i].value );
+    }
+    console.log('colors → ' + JSON.stringify(colors) );
+
+    let Gradient = 'linear-gradient(' + colors.join(', ') + ')';
+    console.log(Gradient);
+    ball.style.backgroundImage = Gradient;
+});
+
+let btnAddColorGR = document.querySelector('.addColorGR');
+let template = document.querySelector('#template').content;
+let newItemTemplate = template.querySelector('.input');
+let i = 2;
+
+btnAddColorGR.addEventListener('click', function(evt) {
+    i += 1;
+    evt.preventDefault();
+    let form = document.querySelector('.form');
+    let task = newItemTemplate.cloneNode(true);
+    task.classList.add('colorGR');
+    task.classList.add('color' + i);
+    form.append(task);
+
+});
+
+
+
+
+function fun1() {
+    let rng=document.getElementById('r1'); //rng - это Input
+    let p=document.getElementById('one'); // p - абзац
+    p.innerHTML=rng.value;
+
+    ball.style.width = rng.value + 'px';
+}
+
+function fun2() {
+    let rng=document.getElementById('r2'); //rng - это Input
+    let p=document.getElementById('two'); // p - абзац
+    p.innerHTML=rng.value;
+
+    ball.style.height = rng.value + 'px';
+}
+
+function fun3() {
+    let rng=document.getElementById('r3'); //rng - это Input
+    let p=document.getElementById('tree'); // p - абзац
+    p.innerHTML=rng.value;
+
+    ball.style.borderRadius = rng.value + '%';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
